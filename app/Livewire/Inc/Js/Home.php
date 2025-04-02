08bef2037b09b00edab160bb59355aa0f42b5024
tranlehuyhoang <tranlehuyhoang13052004@gmail.com>
2025-04-02 14:38:48 +0700

    abdbbd

---
+++ app/Livewire/Inc/Js/Home.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Inc\Js;
+
+use Livewire\Component;
+
+class Home extends Component
+{
+    public function render()
+    {
+        return view('livewire.inc.js.home');
+    }
+}
